226cbb5a4833a581f8497800931c30ba4d61c685
WO <wo@kotocoin.info>
2018-02-17 21:19:43 +0900

    Zcash -> Koto part 2

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var bitcore = module.exports;
 bitcore.version = 'v' + require('./package.json').version;
 bitcore.versionGuard = function(version) {
   if (version !== undefined) {
-    var message = 'More than one instance of bitcore-lib-zcash found. ' +
-      'Please make sure to require bitcore-lib-zcash and check that submodules do' +
-      ' not also include their own bitcore-lib-zcash dependency.';
+    var message = 'More than one instance of bitcore-lib-koto found. ' +
+      'Please make sure to require bitcore-lib-koto and check that submodules do' +
+      ' not also include their own bitcore-lib-koto dependency.';
     // TODO: put this back if we start versioning again
     //throw new Error(message);
   }
